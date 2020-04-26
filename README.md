8f72c42283452bdacedd808b2e17343cea3f4c9a
Panuhin Nikita <79215987151@mail.ru>
2020-04-26 13:14:12 +0300

    Parsing system update, problemset parsing

--- README.md
+++ README.md
@@ -1,22 +1,32 @@
 # EasyCP
+
 EasyCP is a plugin for ST3, which can parse the inputted url and scrap out the sample input and sample output testcases. Further it can run 
 your code on those sample inputs and compares the output so obtained with the given sample output!  
 
+EascyCP allows you to parse all tasks from the problemset at once, so in 2 clicks you can get all tests from the contest!
+
 ### Requirements
 
-- Languages supported: Java, C++, Python 3
-- Websites supported: **[Codeforces](https://codeforces.com "Visit codeforces.com")**
+-  Languages supported: **Java**, **C++**, **Python 3**
+-  Websites supported: **[Codeforces](https://codeforces.com "Visit codeforces.com")**
 
 ### Usage
 
 First of all, go to `Preferences > Package Settings > EasyCP settings` and edit compile and run commands. 
 
-- Go to `Easy CP -> Parse Test-cases` in the menu bar, to scrap sample testcases.
-- Enter the url in the "Input URL" box at the bottom of the screen.
-- Go to `Easy CP -> Compile` in the menu bar to compile your code.
-- Go to `Easy CP -> Run` in the menu bar to execute your code on the sample testcases. 
+-  Go to `Easy CP -> Parse Test-cases` in the menu bar, to scrap sample testcases.
+-  Enter the url in the "Input URL" box at the bottom of the screen.
+-  Go to `Easy CP -> Compile` in the menu bar to compile your code.
+-  Go to `Easy CP -> Run` in the menu bar to execute your code on the sample testcases.
+
+>  **NOTE:**
+>
+>  If you parse only one task, test cases will be associated with the current file name, for example if file name is `Task_B`, then test cases folder will be called `EasyCP_Task_B`.
+>
+>  If you parse the entire problemset, then test cases will be created with file names equal to the letters of the problems, so you have to name your files accordingly, for example, `A.py`,` E.cpp`, `D.java` (not `Tesk_E.py` or `f.cpp`).
+>
+> However, you can always change names simply by renaming the EasyCP folder, for example, changing `EasyCP_A` => `EasyCP_mytask` and `A.cpp` => `mytask.cp` will work as well.
 
 ---
 
-### NOTE:
-This project is under active development. 
+##### *License: MIT*
--- easycp.py
+++ easycp.py
@@ -1,13 +1,24 @@
 import sublime
 import sublime_plugin
 
-from .cfparser import CFParser
 import os
+from . import easycp_parser as Parser
+from re import match as re_match, compile as re_compile
 import subprocess
-from urllib.request import urlopen
 from urllib.parse import urlparse
 from itertools import zip_longest
 from threading import Thread, Lock
+
+
+REGEX = {
+    "cf_problem": re_compile(
+        r"^.*[\/\\]*(?:contest[\/\\]+\d+[\/\\]+problem|problemsets?[\/\\]+(?:.+[\/\\]+)?problem[\/\\]+\d+)[\/\\]+(?:[A-Z]|\d)+$"
+    ),
+    "cf_problemset": re_compile(
+        r"^.*[/\\]*contest[/\\]+\d+[/\\]*$"
+    )
+}
+TEST_FILENAME = "test"
 
 
 def mkpath(*paths) -> str:
@@ -139,8 +150,8 @@ class EasycpRunCommand(Environment):
                     with open(mkpath(self.input_dir, test_file), 'r') as in_file, \
                             open(mkpath(self.output_dir, test_file), 'r') as out_file, \
                             open(mkpath(self.myout_dir, test_file), 'r') as myout_file:
-                        out_data = out_file.read()
-                        myout_data = myout_file.read()
+                        out_data = out_file.read().strip()
+                        myout_data = myout_file.read().strip()
                         panel_print("Input:\n{}\nExpected Output:\n{}\nYour Output:\n{}\nStatus: {}\n\n".format(
                             in_file.read(), out_data, myout_data,
                             "Passed Successfuly" if compare_output(out_data, myout_data) else "FAILED"
@@ -284,35 +295,49 @@ class EasycpParseUrlCommand(Environment):
 
         def on_done(url):
 
+            url_parts = urlparse(url, allow_fragments=True)
+
             # Checking URL
             if "codeforces" not in urlparse(url, allow_fragments=False).netloc:
                 sublime.error_message("EasyCP supports only codefores.com")
                 raise
 
-            # Creating input and output directories
+            if re_match(REGEX["cf_problem"], url_parts.path):
+                test_cases = Parser.CFProblem(url)
 
-            input_dir = mkpath(working_dir, "EasyCP_" + file_name, "input")
+                store_problem(test_cases)
+
+            elif re_match(REGEX["cf_problemset"], url_parts.path):
+                problems = Parser.CFProblemset(url)
+
+                for task_name in problems:
+                    store_problem(problems[task_name], task_name)
+
+            else:
+                sublime.error_message("EasyCP can not recognize Codeforces URL")
+                raise
+
+        def store_problem(test_cases, name=file_name):
+
+            # Creating input and output directories
+            input_dir = mkpath(working_dir, "EasyCP_" + name, "input")
             if not os.path.exists(input_dir):
                 os.makedirs(input_dir)
-            output_dir = mkpath(working_dir, "EasyCP_" + file_name, "output")
+            output_dir = mkpath(working_dir, "EasyCP_" + name, "output")
             if not os.path.exists(output_dir):
                 os.makedirs(output_dir)
 
             # Finding the largest number already used
             num_tests = 1
-            while os.path.exists(mkpath(input_dir, "test" + str(num_tests))):
+            while os.path.exists(mkpath(input_dir, TEST_FILENAME + str(num_tests))):
                 num_tests += 1
 
-            # Getting HTML
-            try:
-                html = urlopen(url).read()
-            except Exception:
-                sublime.error_message("EasyCP: URL Error")
-                raise
-
-            # Parsing test-cases
-            parser = CFParser(input_dir, output_dir, num_tests)
-            parser.feed(html.decode("utf-8"))
+            for test_case in test_cases:
+                with open(mkpath(input_dir, TEST_FILENAME + str(num_tests)), 'w', encoding="utf-8") as input_file:
+                    input_file.write(test_case[0])
+                with open(mkpath(output_dir, TEST_FILENAME + str(num_tests)), 'w', encoding="utf-8") as input_file:
+                    input_file.write(test_case[1])
+                num_tests += 1
 
         sublime.active_window().show_input_panel("Insert URL", "",
                                                  lambda url: sublime.set_timeout_async(lambda: on_done(url), 0),
